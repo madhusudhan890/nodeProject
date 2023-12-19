4a9f4f4a14d9f907c8291f701ae75cfc9aa99e45
Madhu Sudhan <n160452@rguktn.ac.in>
2023-12-19 10:53:07 +0530

    signup changes

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Service = require("../services/services");
 const route = Router();
 
+route.post("/signup", Service.signUp);
 route.get(
   "/",
   Service.fetchData // fetching data without using cache / index values
--- services/services.js
+++ services/services.js
@@ -1,5 +1,20 @@
 const User = require("../models/userModel");
 
+exports.signUp = async (req, res) => {
+  try {
+    const { userName, password, email } = req.body;
+    const data = await User.create({
+      userName: userName,
+      password: password,
+      email: email,
+    });
+    res.status(200).json({
+      message: "Successfull",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 exports.fetchData = async (req, res) => {
   try {
     let data = await User.find({}, { userName: 1, email: 1, userCode: 1 });
